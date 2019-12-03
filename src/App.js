438091e34324f55abf9e748055ac7f9133b4cad1
Max Zakzook <mzakzook@me.com>
2019-12-03 13:04:32 -0800

    Add components and functions for login/create_user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
+import api from './services/api';
+import Login from './Login';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { auth: { currentUser: {} } };
+
+  componentDidMount() {
+    console.log('CDM in APP');
+    const token = localStorage.getItem('token');
+    if (token) {
+      api.auth.getCurrentUser().then(user => {
+        const currentUser = { currentUser: user };
+        debugger
+        this.setState({ auth: currentUser });
+      });
+    }
+  }
+
+  handleLogin = user => {
+    const currentUser = { currentUser: user };
+    localStorage.setItem('token', user.token);
+    
+    this.setState({ auth: currentUser });
+  };
+
+  handleLogout = () => {
+    localStorage.removeItem('token');
+    this.setState({ auth: { currentUser: {} } });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        
+        <div id="content" className="ui container">
+       
+   
+                  <Login handleLogin={this.handleLogin} />
+          
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
