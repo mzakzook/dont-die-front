996354d50e0bf7715e40905d73d89acea6cb87ee
github username <github email address>
2019-12-04 09:58:09 -0800

    NavBar functional except for SignUp

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
 const API_ROOT = `http://localhost:3001/api/v1`;
-
 const token = localStorage.getItem('token');
 
 const headers = {
@@ -13,6 +12,20 @@ const headers = {
 //     res.json()
 //   );
 // };
+
+const create = (fullname, username, password) => {
+  return fetch(`${API_ROOT}/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accepts: 'application/json',
+      Authorization: token
+    },
+    body: JSON.stringify({name: fullname, username: username, password: password })
+  })
+  .then(resp => resp.json())
+  .then(json => console.log(json))
+}
 
 const login = (username, password) => {
   return fetch(`${API_ROOT}/auth/`, {
@@ -31,6 +44,7 @@ const getCurrentUser = () => {
 
 export default {
   auth: {
+    create,
     login,
     getCurrentUser
   }
