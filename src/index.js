9aa237c3f91d55a1e239367c233c83027fcbe4d6
Max Zakzook <mzakzook@me.com>
2019-12-05 16:10:32 -0800

    we're awesome

--- src/index.js
+++ src/index.js
@@ -4,24 +4,24 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import 'typeface-roboto';
 import DiscoverPage from './components/DiscoverPage/DiscoverPage';
-import Login from './Login'
+import Login from './components/Login'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 
 
 
-// ReactDOM.render((
-//     <Router>
-//       <Route exact path="/login" component={Login} />
-//       <Route exact path="/discover" component={DiscoverPage} />
-//     </Router>),
-//     document.getElementById('root')
-// );
+ReactDOM.render((
+    <Router>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/discover" component={DiscoverPage} />
+    </Router>),
+    document.getElementById('root')
+);
 
-ReactDOM.render(<BrowserRouter>
-  <App />
-</BrowserRouter>, document.getElementById('root'));
+// ReactDOM.render(<BrowserRouter>
+//   <App />
+// </BrowserRouter>, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
